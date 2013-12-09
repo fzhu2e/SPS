b9596cf26257424553ee769451bdc65dfa7aaac0
Lyric <lyricorpse@gmail.com>
2013-12-09 22:12:19 +0800

    Simplify tendency_module

--- sp_module_tendency.f90
+++ sp_module_tendency.f90
@@ -23,130 +23,33 @@ TYPE(mainvar), INTENT(IN) :: Main
 TYPE(grid), INTENT(IN) :: uGrid, wGrid, piGrid, virGrid
 REAL(kd), DIMENSION(ims:ime,kms:kme), INTENT(OUT) :: tend_u
 !=================================================
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: F_u
-!-------------------------------------------------
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: uPuPx_u, wPuPz_u
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: fa, fb, fc, fd, fe, ff
-!-------------------------------------------------
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: rhou_pi = undef, rhouu_pi = undef
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: PrhouPx_u = undef, PrhouuPx_u = undef
-
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: rhow_vir = undef, rhowu_vir = undef
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: PrhowPz_u = undef, PrhowuPz_u = undef
-
 REAL(kd), DIMENSION(ims:ime,kms:kme) :: Ppi_1Px_u = undef
-
 REAL(kd), DIMENSION(ims:ime,kms:kme) :: P2uPx2_u = undef, P2uPz2_u = undef
+
+REAL(kd), DIMENSION(ims:ime,kms:kme) :: A_u = undef, P_u = undef, D_u = undef
 !-------------------------------------------------
 INTEGER :: i, k
 !=================================================
-! 1. F_u = - u p.u/p.x - w p.u/p.z + fv   (fv = 0.)
-! 1.1. - u p.u/p.x = - 1/rho (p.rhouu/p.x - u p.rhou/p.x)
-! 1.2. - w p.u/p.z = - 1/rho (p.rhouw/p.z - u p.rhow/p.z)
-!-------------------------------------------------
-! pi-grid - Middle-vars can be calculated on boundaries.
-!-------------------------------------------------
-CALL set_area_pi
-CALL set_area_expand(expand)
+CALL calc_advection_u(Main%u,A_u,uGrid,wGrid,piGrid,virGrid)
 
-!OMP PARALLEL DO
-DO k = kmin, kmax
-	DO i = imin, imax
-		rhou_pi(i,k) = piGrid%rho_0(i,k)*piGrid%u(i,k)
-	END DO
-END DO
-!OMP END PARALLEL DO
-
-SELECT CASE (AdvectionScheme)
-CASE (5)
-	!OMP PARALLEL DO
-	DO k = kmin, kmax
-		DO i = imin, imax
-			fa(i,k) = Main%u(i-1,k) + Main%u(i,k)
-			fb(i,k) = Main%u(i-2,k) + Main%u(i+1,k)
-			fc(i,k) = Main%u(i-3,k) + Main%u(i+2,k)
-			rhouu_pi(i,k) = rhou_pi(i,k)/60.*(37*fa(i,k) - 8*fb(i,k) + fc(i,k))
-			fd(i,k) = Main%u(i,k) - Main%u(i-1,k)
-			fe(i,k) = Main%u(i+1,k) - Main%u(i-2,k)
-			ff(i,k) = Main%u(i+2,k) - Main%u(i-3,k)
-			rhouu_pi(i,k) = rhouu_pi(i,k) - ABS(piGrid%u(i,k))/60.*(10*fd(i,k) - 5*fe(i,k) + ff(i,k))
-		END DO
-	END DO
-	!OMP END PARALLEL DO
-CASE DEFAULT
-	STOP "Wrong advection scheme!!!"
-END SELECT
-!-------------------------------------------------
-! v(virtual)-grid
-!-------------------------------------------------
-CALL set_area_vir
-CALL set_area_expand(expand)
-
-!OMP PARALLEL DO
-DO k = kmin, kmax
-	DO i = imin, imax
-		rhow_vir(i,k) = virGrid%rho_0(i,k)*virGrid%w(i,k)
-	END DO
-END DO
-!OMP END PARALLEL DO
-
-SELECT CASE (AdvectionScheme)
-CASE (5)
-	!OMP PARALLEL DO
-	DO k = kmin, kmax
-		DO i = imin, imax
-			fa(i,k) = Main%u(i,k) + Main%u(i,k-1)
-			fb(i,k) = Main%u(i,k+1) + Main%u(i,k-2)
-			fc(i,k) = Main%u(i,k+2) + Main%u(i,k-3)
-			rhowu_vir(i,k) = rhow_vir(i,k)/60.*(37*fa(i,k) - 8*fb(i,k) + fc(i,k))
-			fd(i,k) = Main%u(i,k) - Main%u(i,k-1)
-			fe(i,k) = Main%u(i,k+1) - Main%u(i,k-2)
-			ff(i,k) = Main%u(i,k+2) - Main%u(i,k-3)
-			rhowu_vir(i,k) = rhowu_vir(i,k) - ABS(virGrid%w(i,k))/60.*(10*fd(i,k) - 5*fe(i,k) + ff(i,k))
-		END DO
-	END DO
-	!OMP END PARALLEL DO
-CASE DEFAULT
-	STOP "Wrong advection scheme!!!"
-END SELECT
-!-------------------------------------------------
-! u-grid
-!-------------------------------------------------
-CALL ppx_u(rhou_pi,PrhouPx_u)
-CALL ppx_u(rhouu_pi,PrhouuPx_u)
-CALL ppzeta_u(rhow_vir,PrhowPz_u)
-CALL ppzeta_u(rhowu_vir,PrhowuPz_u)
 CALL ppx_u(Main%pi_1,Ppi_1Px_u)
 
 CALL set_area_u
 !OMP PARALLEL DO
 DO k = kmin, kmax
 	DO i = imin, imax
-		
-		uPuPx_u(i,k) = 1./uGrid%rho_0(i,k)*(PrhouuPx_u(i,k) - Main%u(i,k)*PrhouPx_u(i,k))
-		wPuPz_u(i,k) = 1./uGrid%rho_0(i,k)*(PrhowuPz_u(i,k) - Main%u(i,k)*PrhowPz_u(i,k))
-	
-		F_u(i,k) = - uPuPx_u(i,k) - wPuPz_u(i,k)
-		tend_u(i,k) = F_u(i,k) - Cp*uGrid%theta_0(i,k)*Ppi_1Px_u(i,k)
+		P_u(i,k) = - Cp*uGrid%theta_0(i,k)*Ppi_1Px_u(i,k)
+
+		P2uPx2_u(i,k) = (Main%u(i+1,k) + Main%u(i-1,k) - 2*Main%u(i,k))/dx/dx
+		P2uPz2_u(i,k) = (Main%u(i,k+1) + Main%u(i,k-1) - 2*Main%u(i,k))/dz/dz
+		D_u(i,k) = Km*(P2uPx2_u(i,k) + P2uPz2_u(i,k))
+
+		tend_u(i,k) = A_u(i,k) + P_u(i,k) + D_u(i,k)
 	END DO
 END DO
 !OMP END PARALLEL DO
-
-IF (RunCase == 1 .OR. RunCase == 2) THEN
-	!OMP PARALLEL DO
-	DO k = kmin, kmax
-		DO i = imin, imax
-			P2uPx2_u(i,k) = (Main%u(i+1,k) + Main%u(i-1,k) - 2*Main%u(i,k))/dx/dx
-			P2uPz2_u(i,k) = (Main%u(i,k+1) + Main%u(i,k-1) - 2*Main%u(i,k))/dz/dz
-			
-			tend_u(i,k) = tend_u(i,k) + Km*(P2uPx2_u(i,k) + P2uPz2_u(i,k)) ! Add diffusion term.
-		END DO
-	END DO
-	!OMP END PARALLEL DO
-END IF
-	
 !-------------------------------------------------
-IF (ANY(ISNAN(F_u(its:ite,kts:kte)))) STOP "SOMETHING IS WRONG WITH F_u!!!"
+IF (ANY(ISNAN(tend_u(its:ite,kts:kte)))) STOP "SOMETHING IS WRONG WITH tend_u!!!"
 !=================================================
 END SUBROUTINE tendency_u
 !=================================================
@@ -158,130 +61,37 @@ TYPE(mainvar), INTENT(IN) :: Main
 TYPE(grid), INTENT(IN) :: uGrid, wGrid, piGrid, virGrid
 REAL(kd), DIMENSION(ims:ime,kms:kme), INTENT(OUT) :: tend_w
 !=================================================
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: F_w
+REAL(kd), DIMENSION(ims:ime,kms:kme) :: A_w = undef, B_w = undef, P_w = undef, D_w = undef
 !-------------------------------------------------
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: uPwPx_w, wPwPz_w
 REAL(kd), DIMENSION(ims:ime,kms:kme) :: fa, fb, fc, fd, fe, ff
 !-------------------------------------------------
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: rhou_vir = undef, rhouw_vir = undef
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: PrhouPx_w = undef, PrhouwPx_w = undef
-
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: rhow_pi = undef, rhoww_pi = undef
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: PrhowwPz_w = undef, Ppi_1Pz_w = undef
-
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: PrhowPz_w = undef
+REAL(kd), DIMENSION(ims:ime,kms:kme) :: Ppi_1Pz_w = undef
 
 REAL(kd), DIMENSION(ims:ime,kms:kme) :: P2wPx2_w = undef, P2wPz2_w = undef
 !-------------------------------------------------
 INTEGER :: i, k
 !=================================================
-! 2. F_w = - u p.w/p.x - w p.w/p.z + g(theta_1/theta_0)
-! 2.1. - u p.w/p.x = - 1/rho (p.rhouw/p.x - w p.rhou/p.x)
-! 2.2. - w p.w/p.z = - 1/rho (p.rhoww/p.z - w p.rhow/p.z)
-!-------------------------------------------------
-! v(virtual)-grid
-!-------------------------------------------------
-CALL set_area_vir
-CALL set_area_expand(expand)
-
-!OMP PARALLEL DO
-DO k = kmin, kmax
-	DO i = imin, imax
-		rhou_vir(i,k) = virGrid%rho_0(i,k)*virGrid%u(i,k)
-	END DO
-END DO
-!OMP END PARALLEL DO
-
-SELECT CASE (AdvectionScheme)
-CASE (5)
-	!OMP PARALLEL DO
-	DO k = kmin, kmax
-		DO i = imin, imax
-			fa(i,k) = Main%w(i,k) + Main%w(i+1,k)
-			fb(i,k) = Main%w(i-1,k) + Main%w(i+2,k)
-			fc(i,k) = Main%w(i-2,k) + Main%w(i+3,k)
-			rhouw_vir(i,k) = rhou_vir(i,k)/60.*(37*fa(i,k) - 8*fb(i,k) + fc(i,k))
-			fd(i,k) = Main%w(i+1,k) - Main%w(i,k)
-			fe(i,k) = Main%w(i+2,k) - Main%w(i-1,k)
-			ff(i,k) = Main%w(i+3,k) - Main%w(i-2,k)
-			rhouw_vir(i,k) = rhouw_vir(i,k) - ABS(virGrid%u(i,k))/60.*(10*fd(i,k) - 5*fe(i,k) + ff(i,k))
-		END DO
-	END DO
-	!OMP END PARALLEL DO
-CASE DEFAULT
-	STOP "Wrong advection scheme!!!"
-END SELECT
-!-------------------------------------------------
-! pi-grid
-!-------------------------------------------------
-CALL set_area_pi
-CALL set_area_expand(expand)
-
-!OMP PARALLEL DO
-DO k = kmin, kmax
-	DO i = imin, imax
-		rhow_pi(i,k) = piGrid%rho_0(i,k)*piGrid%w(i,k)
-	END DO
-END DO
-!OMP END PARALLEL DO
-
-SELECT CASE (AdvectionScheme)
-CASE (5)
-	!OMP PARALLEL DO
-	DO k = kmin, kmax
-		DO i = imin, imax
-			fa(i,k) = Main%w(i,k) + Main%w(i,k+1)
-			fb(i,k) = Main%w(i,k-1) + Main%w(i,k+2)
-			fc(i,k) = Main%w(i,k-2) + Main%w(i,k+3)
-			rhoww_pi(i,k) = rhow_pi(i,k)/60.*(37*fa(i,k) - 8*fb(i,k) + fc(i,k))
-	
-			fd(i,k) = Main%w(i,k+1) - Main%w(i,k)
-			fe(i,k) = Main%w(i,k+2) - Main%w(i,k-1)
-			ff(i,k) = Main%w(i,k+3) - Main%w(i,k-2)
-			rhoww_pi(i,k) = rhoww_pi(i,k) - ABS(piGrid%w(i,k))/60.*(10*fd(i,k) - 5*fe(i,k) + ff(i,k))
-		END DO
-	END DO
-	!OMP END PARALLEL DO
-CASE DEFAULT
-	STOP "Wrong advection scheme!!!"
-END SELECT
-!-------------------------------------------------
-! w-grid 
-!-------------------------------------------------
-CALL ppx_w(rhou_vir,PrhouPx_w)
-CALL ppx_w(rhouw_vir,PrhouwPx_w)
-CALL ppzeta_w(rhow_pi,PrhowPz_w)
-CALL ppzeta_w(rhoww_pi,PrhowwPz_w)
+CALL calc_advection_w(Main%w,A_w,uGrid,wGrid,piGrid,virGrid)
 CALL ppzeta_w(Main%pi_1,Ppi_1Pz_w)
 
 CALL set_area_w
 !OMP PARALLEL DO
 DO k = kmin, kmax
 	DO i = imin, imax
+		B_w(i,k) = g*wGrid%theta_1(i,k)/wGrid%theta_0(i,k)
 
-		uPwPx_w(i,k) = 1./wGrid%rho_0(i,k)*(PrhouwPx_w(i,k) - Main%w(i,k)*PrhouPx_w(i,k))
-		wPwPz_w(i,k) = 1./wGrid%rho_0(i,k)*(PrhowwPz_w(i,k) - Main%w(i,k)*PrhowPz_w(i,k))
+		P_w(i,k) = - Cp*wGrid%theta_0(i,k)*Ppi_1Pz_w(i,k)
 
-		F_w(i,k) = - uPwPx_w(i,k) - wPwPz_w(i,k) + g*wGrid%theta_1(i,k)/wGrid%theta_0(i,k)
-		tend_w(i,k) = F_w(i,k) - Cp*wGrid%theta_0(i,k)*Ppi_1Pz_w(i,k)
+		P2wPx2_w(i,k) = (Main%w(i+1,k) + Main%w(i-1,k) - 2*Main%w(i,k))/dx/dx
+		P2wPz2_w(i,k) = (Main%w(i,k+1) + Main%w(i,k-1) - 2*Main%w(i,k))/dz/dz
+		D_w(i,k) = Km*(P2wPx2_w(i,k) + P2wPz2_w(i,k))
+
+		tend_w(i,k) = A_w(i,k) + B_w(i,k) + P_w(i,k) + D_w(i,k)
 	END DO
 END DO
 !OMP END PARALLEL DO
-
-IF (RunCase == 1 .OR. RunCase == 2) THEN
-	!OMP PARALLEL DO
-	DO k = kmin, kmax
-		DO i = imin, imax
-			P2wPx2_w(i,k) = (Main%w(i+1,k) + Main%w(i-1,k) - 2*Main%w(i,k))/dx/dx
-			P2wPz2_w(i,k) = (Main%w(i,k+1) + Main%w(i,k-1) - 2*Main%w(i,k))/dz/dz
-			
-			tend_w(i,k) = tend_w(i,k) + Km*(P2wPx2_w(i,k) + P2wPz2_w(i,k)) ! Add diffusion term.
-		END DO
-	END DO
-	!OMP END PARALLEL DO
-END IF
 !-------------------------------------------------
-IF (ANY(ISNAN(F_w(its:ite,kts:kte)))) STOP "SOMETHING IS WRONG WITHT F_w!!!"
+IF (ANY(ISNAN(tend_w(its:ite,kts:kte)))) STOP "SOMETHING IS WRONG WITHT tend_w!!!"
 !=================================================
 END SUBROUTINE tendency_w
 !=================================================
@@ -304,7 +114,6 @@ INTEGER :: i, k
 !-------------------------------------------------
 CALL set_area_u
 CALL set_area_expand(expand)
-
 !OMP PARALLEL DO
 DO k = kmin, kmax
 	DO i = imin, imax
@@ -315,7 +124,6 @@ END DO
 
 CALL set_area_w
 CALL set_area_expand(expand)
-
 !OMP PARALLEL DO
 DO k = kmin, kmax
 	DO i = imin, imax
@@ -331,7 +139,6 @@ CALL set_area_pi
 !OMP PARALLEL DO
 DO k = kmin, kmax
 	DO i = imin, imax
-		
 		F_pi(i,k) = - cs*cs/Cp/piGrid%rho_0(i,k)/piGrid%theta_0(i,k)/piGrid%theta_0(i,k)*(PurhothetaPx_pi(i,k) + PwrhothetaPz_pi(i,k))
 		tend_pi_1(i,k) = F_pi(i,k)
 	END DO
@@ -350,129 +157,27 @@ TYPE(mainvar), INTENT(IN) :: Main
 TYPE(grid), INTENT(IN) :: uGrid, wGrid, piGrid, virGrid
 REAL(kd), DIMENSION(ims:ime,kms:kme), INTENT(OUT) :: tend_theta
 !=================================================
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: F_theta
+REAL(kd), DIMENSION(ims:ime,kms:kme) :: A_theta = undef, D_theta = undef
 !-------------------------------------------------
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: uPthetaPx_w, wPthetaPz_w
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: fa, fb, fc, fd, fe, ff
-!-------------------------------------------------
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: rhou_vir = undef, rhow_pi = undef
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: PrhouPx_w = undef, PrhowPz_w = undef
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: rhoutheta_vir = undef, rhowtheta_pi = undef
-REAL(kd), DIMENSION(ims:ime,kms:kme) :: PrhouthetaPx_w = undef, PrhowthetaPz_w = undef
 REAL(kd), DIMENSION(ims:ime,kms:kme) :: P2thetaPx2_w = undef, P2thetaPz2_w = undef
 INTEGER :: i, k
 !=================================================
-! 3. F_theta = - u p.theta/p.x - w p.theta/p.z
-! 3.1. - u p.theta/p.x = - 1/rho (p.rhoutheta/p.x - theta p.rhou/p.x)
-! 3.2. - w p.w/p.z = - 1/rho (p.rhothetaw/p.z - theta p.rhow/p.z)
-!-------------------------------------------------
-! pi-grid
-!-------------------------------------------------
-CALL set_area_pi
-CALL set_area_expand(expand)
-
-!OMP PARALLEL DO
-DO k = kmin, kmax
-	DO i = imin, imax
-		rhow_pi(i,k) = piGrid%rho_0(i,k)*piGrid%w(i,k)
-	END DO
-END DO
-!OMP END PARALLEL DO
-
-!-------------------------------------------------
-! v(virtual)-grid
-!-------------------------------------------------
-CALL set_area_vir
-CALL set_area_expand(expand)
-
-!OMP PARALLEL DO
-DO k = kmin, kmax
-	DO i = imin, imax
-		rhou_vir(i,k) = virGrid%rho_0(i,k)*virGrid%u(i,k)
-	END DO
-END DO
-!OMP END PARALLEL DO
-
-SELECT CASE (AdvectionScheme)
-CASE (5)
-	!OMP PARALLEL DO
-	DO k = kmin, kmax
-		DO i = imin, imax
-			fa(i,k) = Main%theta(i,k) + Main%theta(i+1,k)
-			fb(i,k) = Main%theta(i-1,k) + Main%theta(i+2,k)
-			fc(i,k) = Main%theta(i-2,k) + Main%theta(i+3,k)
-			rhoutheta_vir(i,k) = rhou_vir(i,k)/60.*(37*fa(i,k) - 8*fb(i,k) + fc(i,k))
-			fd(i,k) = Main%theta(i+1,k) - Main%theta(i,k)
-			fe(i,k) = Main%theta(i+2,k) - Main%theta(i-1,k)
-			ff(i,k) = Main%theta(i+3,k) - Main%theta(i-2,k)
-			rhoutheta_vir(i,k) = rhoutheta_vir(i,k) - ABS(virGrid%u(i,k))/60.*(10*fd(i,k) - 5*fe(i,k) + ff(i,k))
-		END DO
-	END DO
-	!OMP END PARALLEL DO
-CASE DEFAULT
-	STOP "Wrong advection scheme!!!"
-END SELECT
-!-------------------------------------------------
-! pi-grid
-!-------------------------------------------------
-CALL set_area_pi
-CALL set_area_expand(expand)
-
-SELECT CASE (AdvectionScheme)
-CASE (5)
-	!OMP PARALLEL DO
-	DO k = kmin, kmax
-		DO i = imin, imax
-			fa(i,k) = Main%theta(i,k+1) + Main%theta(i,k)
-			fb(i,k) = Main%theta(i,k+2) + Main%theta(i,k-1)
-			fc(i,k) = Main%theta(i,k+3) + Main%theta(i,k-2)
-			rhowtheta_pi(i,k) = rhow_pi(i,k)/60.*(37*fa(i,k) - 8*fb(i,k) + fc(i,k))
-			fd(i,k) = Main%theta(i,k+1) - Main%theta(i,k)
-			fe(i,k) = Main%theta(i,k+2) - Main%theta(i,k-1)
-			ff(i,k) = Main%theta(i,k+3) - Main%theta(i,k-2)
-			rhowtheta_pi(i,k) = rhowtheta_pi(i,k) - ABS(piGrid%w(i,k))/60.*(10*fd(i,k) - 5*fe(i,k) + ff(i,k))
-		END DO
-	END DO
-	!OMP END PARALLEL DO
-CASE DEFAULT
-	STOP "Wrong advection scheme!!!"
-END SELECT
-!-------------------------------------------------
-! w-grid - Theta on kts and kte+1 should also be updated.
-!-------------------------------------------------
-CALL ppx_w(rhou_vir,PrhouPx_w)
-CALL ppzeta_w(rhow_pi,PrhowPz_w)
-CALL ppx_w(rhoutheta_vir,PrhouthetaPx_w)
-CALL ppzeta_w(rhowtheta_pi,PrhowthetaPz_w)
+CALL calc_advection_w(Main%theta,A_theta,uGrid,wGrid,piGrid,virGrid)
 
 CALL set_area_w
-
 !OMP PARALLEL DO
 DO k = kmin, kmax
 	DO i = imin, imax
-		uPthetaPx_w(i,k) = 1./wGrid%rho_0(i,k)*(PrhouthetaPx_w(i,k) - Main%theta(i,k)*PrhouPx_w(i,k))
-		wPthetaPz_w(i,k) = 1./wGrid%rho_0(i,k)*(PrhowthetaPz_w(i,k) - Main%theta(i,k)*PrhowPz_w(i,k))
-	
-		F_theta(i,k) = - uPthetaPx_w(i,k) - wPthetaPz_w(i,k)
-		tend_theta(i,k) = F_theta(i,k)
+		P2thetaPx2_w(i,k) = (Main%theta(i+1,k) + Main%theta(i-1,k) - 2*Main%theta(i,k))/dx/dx
+		P2thetaPz2_w(i,k) = (Main%theta(i,k+1) + Main%theta(i,k-1) - 2*Main%theta(i,k))/dz/dz
+		D_theta(i,k) = Kh*(P2thetaPx2_w(i,k) + P2thetaPz2_w(i,k))
+
+		tend_theta(i,k) = A_theta(i,k) + D_theta(i,k)
 	END DO
 END DO
 !OMP END PARALLEL DO
-	
-IF (RunCase == 1 .OR. RunCase == 2) THEN
-	!OMP PARALLEL DO
-	DO k = kmin, kmax
-		DO i = imin, imax
-			P2thetaPx2_w(i,k) = (Main%theta(i+1,k) + Main%theta(i-1,k) - 2*Main%theta(i,k))/dx/dx
-			P2thetaPz2_w(i,k) = (Main%theta(i,k+1) + Main%theta(i,k-1) - 2*Main%theta(i,k))/dz/dz
-			
-			tend_theta(i,k) = F_theta(i,k) + Kh*(P2thetaPx2_w(i,k) + P2thetaPz2_w(i,k)) ! Add diffusion term.
-		END DO
-	END DO
-	!OMP END PARALLEL DO
-END IF
 !-------------------------------------------------
-IF (ANY(ISNAN(F_theta(its:ite,kts:kte)))) STOP "SOMETHING IS WRONG WITH F_theta!!!"
+IF (ANY(ISNAN(tend_theta(its:ite,kts:kte)))) STOP "SOMETHING IS WRONG WITH tend_theta!!!"
 !=================================================
 END SUBROUTINE tendency_theta
 !=================================================
