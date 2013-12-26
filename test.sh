86e619665ab1d7978498e3a680dbad36cc8422c1
Lyric <lyricorpse@gmail.com>
2013-12-26 11:19:04 +0800

    OpenMP enhanced.

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 make clean; make
 ulimit -s unlimited
 #OMP_NUM_THREADS=1
-OMP_NUM_THREADS=2
-#OMP_NUM_THREADS=4
+#OMP_NUM_THREADS=2
+OMP_NUM_THREADS=4
 #OMP_NUM_THREADS=8
 time ./sp_dynamic
 echo "OMP_NUM_THREADS = $OMP_NUM_THREADS"
